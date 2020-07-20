351605fca437ce48e72ba553df8e4ce7e220f081
standstonecraft <stand.stone.complex@gmail.com>
2020-07-20 05:18:16 -0700

    small fix

--- test/exportCoverage.ps1
+++ test/exportCoverage.ps1
@@ -1,5 +1,5 @@
 $resultGuid = (Get-ChildItem .\TestResults -Directory)[0].Name
-echo "generate coverage report of $resultGuid"
+Write-Output "generate coverage report of $resultGuid"
 dotnet tool run reportgenerator `
     "-reports:TestResults\$resultGuid\coverage.cobertura.xml" `
     "-targetdir:TestResults/CoverageReport" `
