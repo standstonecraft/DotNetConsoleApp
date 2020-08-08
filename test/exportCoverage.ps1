0f955e39f33aea1311cae44d7188d0ceb8797ebc
standstonecraft <stand.stone.complex+github@gmail.com>
2020-08-08 03:39:09 -0700

    dotnet tool restore

--- test/exportCoverage.ps1
+++ test/exportCoverage.ps1
@@ -1,5 +1,6 @@
 $resultGuid = (Get-ChildItem .\TestResults -Directory)[0].Name
 Write-Output "generate coverage report of $resultGuid"
+dotnet tool restore
 dotnet tool run reportgenerator `
     "-reports:TestResults\$resultGuid\coverage.cobertura.xml" `
     "-targetdir:TestResults/CoverageReport" `
