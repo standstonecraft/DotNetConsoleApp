b769f3be0d757d1179b34987b326b14fe8ca4054
standstonecraft <stand.stone.complex+github@gmail.com>
2020-08-08 11:47:54 -0700

    test sql sample

--- test/cs/SqlTest.cs
+++ test/cs/SqlTest.cs
@@ -32,7 +32,7 @@ namespace DotNetConsoleApp.Test {
         /// クエリーサンプル
         /// </summary>
         [Fact]
-        public void CreateTest() {
+        public void SqlSampleTest() {
 
             string ddl = @"
             CREATE TABLE #TEMP_SAMPLE_TABLE
@@ -44,14 +44,20 @@ namespace DotNetConsoleApp.Test {
             int createReturn = SqlUtil.DefaultConnection.Execute(ddl);
             Assert.Equal(createReturn, -1);
 
+            // INSERT
             List<Dao.SampleTable> items = new List<Dao.SampleTable>();
             for (int i = 0; i < 3; i++) {
                 items.Add(new Dao.SampleTable(i, "row" + i));
             }
-
-            string dml = "INSERT INTO #TEMP_SAMPLE_TABLE (COL1, COL2) VALUES (@Col1, @Col2)";
-            int insertReturn = SqlUtil.DefaultConnection.Execute(dml, items);
+            string insSql = "INSERT INTO #TEMP_SAMPLE_TABLE (COL1, COL2) VALUES (@Col1, @Col2)";
+            int insertReturn = SqlUtil.DefaultConnection.Execute(insSql, items);
             Assert.Equal(3, insertReturn);
+
+            // SELECT バインド変数付き
+            string selSql = "SELECT COL1, COL2 FROM #TEMP_SAMPLE_TABLE WHERE COL1 > @Cond ORDER BY COL1";
+            List<Dao.SampleTable> selected = SqlUtil.DefaultConnection.Query<Dao.SampleTable>(selSql, new { Cond = 0 }).AsList();
+            Assert.Equal(2, selected.Count);
+            Assert.Equal("row2", selected[1].Col2);
         }
     }
 }
